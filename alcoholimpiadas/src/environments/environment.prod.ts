6e695cc3c84813550a8c2db2539aa808253b00fe
Agustín <payaagustin@gmail.com>
2025-08-27 11:25:16 +0200

    cambios necesarios deploy

--- alcoholimpiadas/src/environments/environment.prod.ts
+++ alcoholimpiadas/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://cuuqamxdflndjrbhtmsz.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1dXFhbXhkZmxuZGpyYmh0bXN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMjk3MTIsImV4cCI6MjA2NzgwNTcxMn0.JZdAXgb2bnQmlHn80swirGbAOOLS2JJxBRzH1fxzB1g' // Usa la clave anon, no service_role
 };
